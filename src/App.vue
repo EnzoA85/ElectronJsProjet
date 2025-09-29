f0f50e2ecb897d4d669a12031054c70785e4a7af
Enzo Archambaud <bib.enzoarchambaud@gmail.com>
2025-09-29 09:33:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>Appli-quons-nous</h1>
+	<ul uk-tab>
+		<li class="uk-active"><router-link to="/">Accueil</router-link></li>
+		<li><router-link to="/page-2">page Two</router-link></li>
+		<li><a href="#">Item</a></li>
+	</ul>
+	<router-view />
+</template>
---
+++ src/views/PageOne.vue
@@ -0,0 +1,23 @@
+<!-- UX/UI de la vue -->
+<template>
+    <h3>Liste de films</h3>
+    <button @click="callApi" class="uk-button uk-button-primary">Appeler API</button>
+
+    <div v-for="movie in movies" :key="movie.title">
+        {{ movie.title }}
+    </div>
+</template>
+
+<!-- Partie logique de la vue -->
+<script setup>
+import { ref } from 'vue';
+
+const movies = ref([]);
+
+async function callApi() {
+    const res = await fetch('https://raw.githubusercontent.com/Chocolaterie/EniWebService/main/api/movies.json');
+    const data = await res.json();
+    // J'alimente les données dans la liste des films statefull
+    movies.value=data;
+}
+</script>
